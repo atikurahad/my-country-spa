cf6bb5657e239d98a4632c9b6e57a551bfb913e1
Atikur Rahaman <atikurahad@gmail.com>
2023-01-30 19:37:30 +0600

    first commit

---
+++ src/Person.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Person() {
+  return (
+    <div>
+      <p>Name: Tamim Iqbal</p>
+      <p>Age: 35</p>
+      <p>Profession: Crickter</p>
+    </div>
+  )
+}
